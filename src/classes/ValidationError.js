2bab0ab3e57c4e6df5cbcaf498eaa9e6fe8cc403
Cameron Welter <camjwelt@gmail.com>
2020-02-25 08:58:31 +0000

    detect manual messages

--- src/classes/ValidationError.js
+++ src/classes/ValidationError.js
@@ -1,24 +1,29 @@
 export default class ValidationError extends Error {
   constructor (assertion, propertyName, propertyValue, parentMeta) {
-    let message = 'Expected argument '
-
-    if (propertyName) {
-      message += `\`${propertyName}\` `
-    }
-
-    if (parentMeta) {
-      message += `of ${parentMeta.type} \`${parentMeta.name}\` `
-    }
-
-    message += `to ${assertion}` ?? 'to be something, but whoever wrote this validator is a pepega.'
-
-    if (propertyValue) {
-      message += `, but got \`${propertyValue}\` instead.`
+    if (assertion && !propertyName && !propertyValue && !parentMeta) {
+      super(assertion) // we've been passed a manual message
     } else {
-      message += '.'
+      let message = 'Expected argument '
+
+      if (propertyName) {
+        message += `\`${propertyName}\` `
+      }
+
+      if (parentMeta) {
+        message += `of ${parentMeta.type} \`${parentMeta.name}\` `
+      }
+
+      message += `to ${assertion}` ?? 'to be something, but whoever wrote this validator is a pepega.'
+
+      if (propertyValue) {
+        message += `, but got \`${propertyValue}\` instead.`
+      } else {
+        message += '.'
+      }
+
+      super(message)
     }
 
-    super(message)
     this.name = 'ValidationError'
   }
 }
